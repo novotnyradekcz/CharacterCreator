9e7ce14f2fecbe37b2a7614529e69b0b5ed5d68f
Radek Novotný <novotnyradekcz@gmail.com>
2023-04-17 21:21:56 +0200

    Added ability scores

--- Character.cs
+++ Character.cs
@@ -10,17 +10,35 @@ namespace CharacterCreator
         private string _name;
         private string _race;
         private string _class;
+        private int _strength;
+        private int _dexterity;
+        private int _constitution;
+        private int _intelligence;
+        private int _wisdom;
+        private int _charisma;
 
         public Character(string name, string race, string characterClass)
         {
             this._name = name;
             this._race = race;
             this._class = characterClass;
+            this._strength = 10;
+            this._dexterity = 10;
+            this._constitution = 10;
+            this._intelligence = 10;
+            this._wisdom = 10;
+            this._charisma = 10;
         }
 
         public void SayHello()
         {
             Console.WriteLine("Ahoj, jmenuji se {0}, jsem {1} {2}.", this._name, this._race, this._class);
+        }
+
+        public void RollAbilityScores()
+        {
+            Random dice = new Random();
+            dice.Next(7);
         }
     }
 }
